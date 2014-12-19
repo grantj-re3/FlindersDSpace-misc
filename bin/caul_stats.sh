cfcc0ac0dffd0283a29a6ca9d3c7e9974e73b7b4
grantj-re3 <grantj-re3@github.fake>
2014-12-19 16:23:02 +1030

    Added more CAUL counts for the specified year

--- bin/caul_stats.sh
+++ bin/caul_stats.sh
@@ -18,6 +18,11 @@ user=$USER	# Database user: Assume same name as the Unix user
 db=dspace	# Database name
 app=`basename $0`
 
+# The default year is the year which was this many days ago.
+# I would like the year-counts to be for last-year for cron jobs on 1st Jan
+# and 1st Feb; else I would like current-year counts.
+num_days_ago=40
+
 ##############################################################################
 # usage_exit(msg) -- Display specified message, then usage-message, then exit
 ##############################################################################
@@ -25,8 +30,9 @@ usage_exit() {
   msg="$1"
   {
     [ "$msg" ] && echo "$msg"
-    echo "Usage:  $app [ COLLECTION_HANDLE ]"
+    echo "Usage:  $app [ YEAR [ COLLECTION_HANDLE ] ]"
     echo "   or:  $app --help|-h"
+    echo "If not specified, YEAR will default to the year which was $num_days_ago days ago."
     echo "Omit the COLLECTION_HANDLE to gather CAUL statistics for all collections."
   } >&2
   exit 1
@@ -39,6 +45,13 @@ usage_exit() {
 # Deal with command line parameters
 [ "$1" = -h -o "$1" = --help ] && usage_exit
 
+if [ "$1" = "" ]; then
+  year=`date -d "$num_days_ago days ago" +%Y`			# Default to this year
+else
+  year="$1"
+fi
+
+shift
 if [ "$1" = "" ]; then
   collection_hdl=""
   collection_clause=""
@@ -78,6 +91,15 @@ with
           )
         )
       )
+  ),
+  year_items as (
+    select item_id
+    from metadatavalue
+    where
+      metadata_field_id =
+        (select metadata_field_id from metadatafieldregistry where element='date' and qualifier='accessioned')
+    group by item_id
+    having min(substring(text_value from 1 for 4)) = '$year'
   )
 
 select
@@ -98,6 +120,16 @@ select
   ) icount_4_notbitstream_total,
   (select count(*) from published_items) icount_7_total,
 
+  '$year' for_year,
+  (select count(*) from published_items where item_id not in
+    (select item_id from bitstream_items) and item_id in
+    (select item_id from year_items)
+  ) icount_5_notbitstream_year,
+  (select count(*) from published_items where item_id in
+    (select item_id from bitstream_items) and item_id in
+    (select item_id from year_items)
+  ) icount_6_bitstream_year,
+
   to_char(now(), 'YYYY-MM-DD HH24:MI:SS') now,
   (case '$collection_hdl'
     when '' then 'All-Collections'
@@ -113,7 +145,7 @@ with
   delimiter ','
   csv
     header
-    force quote now,db_range
+    force quote for_year,now,db_range
 "
 
 descr="This query gives item-counts for CAUL institutional repository
@@ -144,11 +176,20 @@ Column names are defined as follows:
     Total count of published metadata-only items (ie. not having bitstreams).
   icount_7_total:
     Total count of published items (ie. the sum of counts 3 and 4).
+  for_year:
+    The year for the year-counts which follow (ie. counts 5 and 6).
+  icount_5_notbitstream_year:
+    Total count of published metadata-only items (ie. not having bitstreams)
+    for the specified (dc.date.accessioned) year.
+  icount_6_bitstream_year:
+    Total count of published items having bitstreams for the specified
+    (dc.date.accessioned) year.
   now:
     The time this query was run.
   db_range:
     The database range against which this query was run. Eg. Either
-    'All-Collections' or 'Collection-Handle-123456789/26019-Only' (for collection handle 123456789/26019).
+    'All-Collections' or 'Collection-Handle-123456789/26019-Only'
+    (for collection handle 123456789/26019).
 "
 
 ##############################################################################
