03860dda31b587ca84b6af9a569ca9d56990fd74
grantj-re3 <grantj-re3@github.fake>
2015-08-17 17:48:05 +0930

    Add --email switch to df_check.sh: - causes result to be emailed if non-empty - intended use is within cron

--- bin/df_check.sh
+++ bin/df_check.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
-# df_check.sh
 # Issue a warning if any mount point exceeds its specified %-full threshold.
+#
+# Usage 1: df_check.sh			# Display result to STDOUT
+# Usage 2: df_check.sh --email|-e	# Send email if result is not empty
 #
 # Copyright (c) 2014, Flinders University, South Australia. All rights reserved.
 # Contributors: Library, Information Services, Flinders University.
 # See the accompanying LICENSE file (or http://opensource.org/licenses/BSD-3-Clause).
 #
 ##############################################################################
+PATH=/bin:/usr/bin:/usr/local/bin;	export PATH
+
+# mailx: Space separated list of destination email addresses
+email_dest_list="me@example.com you@example.com"
+email_subject="WARNING: Disk too full on `hostname` at `date +%F\ %T`"
+
 # Warning generated if df percent-column is above this percentage
 PERCENT_FULL_DEFAULT_THRESHOLD=80
 
@@ -34,6 +42,7 @@ percent_full_indiv_threshold="
 #     threshold = Value from above config
 #     Display warning if(pct > threshold)
 #   }
+show_if_mountpt_too_full() {
 df -m |
   tr -d % |
   awk 'NR>1 && NF==6 {print} NR>1 && NF==5 {print p $0} {p=$0}' |
@@ -47,4 +56,17 @@ df -m |
 
       [ "$pct" -gt "$threshold" ] && echo "WARNING: $mountpt is $pct% full"
     done
+}
+
+##############################################################################
+# main()
+##############################################################################
+if [ "$1" = "--email" -o "$1" = "-e" ]; then
+  # Send email if result is not empty
+  result=`show_if_mountpt_too_full`
+  [ `echo "$result" |wc -c` -gt 1 ] && echo "$result" |mailx -s "$email_subject" $email_dest_list
+
+else
+  show_if_mountpt_too_full		# Display result to STDOUT
+fi
 
