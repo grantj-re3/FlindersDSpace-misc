b44f34346995a25c1703fd959bc01cc9ba321be2
grantj-re3 <grantj-re3@github.fake>
2019-06-18 17:32:55 +0930

    Update extractItemsByColl_HdlId.sh: - Allow operation with DSpace 5 database - Allow database connection to be defined in an external file - Allow the collection handle to be entered on the command line

--- bin/extractItemsByColl_HdlId.sh
+++ bin/extractItemsByColl_HdlId.sh
@@ -1,15 +1,46 @@
 #!/bin/sh
 #
-# Copyright (c) 2014, Flinders University, South Australia. All rights reserved.
-# Contributors: eResearch@Flinders, Library, Information Services, Flinders University.
+# Copyright (c) 2014-2019, Flinders University, South Australia. All rights reserved.
+# Contributors: Library, Corporate Services, Flinders University.
 # See the accompanying LICENSE file (or http://opensource.org/licenses/BSD-3-Clause).
 #
 ##############################################################################
 user=$USER	# Database user: Assume same name as the Unix user
 db=dspace	# Database name
 
-# Handle of the collection which we are querying
-collection_hdl="123456789/6071"
+app=`basename $0`
+
+##############################################################################
+# Optionally override the above psql_connect_opts variable.
+psql_env_fname=`dirname $0`/psql_connect_env.sh	# Path to environment-file
+[ -f $psql_env_fname ] && . $psql_env_fname
+
+##############################################################################
+# DSpace resource_type_id
+# See https://github.com/DSpace/DSpace/blob/master/dspace-api/src/main/java/org/dspace/core/Constants.java
+TYPE_ITEM=2
+TYPE_COLLECTION=3
+
+##############################################################################
+# usage_exit(msg) -- Display specified message, then usage-message, then exit
+##############################################################################
+usage_exit() {
+  msg="$1"
+  {
+    [ "$msg" ] && echo "$msg"
+    echo "Usage:  $app  COLLECTION_HANDLE"
+    echo "   or:  $app --help|-h"
+    echo "where COLLECTION_HANDLE is of the form 123456789/9999"
+  } >&2
+  exit 1
+}
+
+##############################################################################
+# Main
+##############################################################################
+# Deal with command line parameters
+[ "$1" = -h -o "$1" = --help -o "$1" = "" ] && usage_exit
+collection_hdl="$1"
 
 # The item_id and title columns may be handy for debugging.
 # If you don't need them, then either remove the corresponding lines from
@@ -17,21 +48,28 @@ collection_hdl="123456789/6071"
 # favourite spreadsheet app.
 sql="
 select
-  (select 'http://hdl.handle.net/'||handle from handle where resource_type_id=2 and resource_id=i.item_id) handle,
-  (
-    select text_value from metadatavalue where item_id=i.item_id and metadata_field_id =
-      (select metadata_field_id from metadatafieldregistry where element='identifier' and qualifier is null)
-  ) identifier,
+  (select 'http://hdl.handle.net/'||handle from handle where resource_type_id=$TYPE_ITEM and resource_id=i.item_id) handle,
+
+  array_to_string(array(
+    select text_value from metadatavalue where resource_type_id=$TYPE_ITEM and resource_id=i.item_id and metadata_field_id=
+      (select metadata_field_id from metadatafieldregistry where element='identifier' and qualifier is null and metadata_schema_id=
+        (select metadata_schema_id from metadataschemaregistry where short_id='dc')
+      )
+  ), '||') identifiers,
+
   i.item_id,
-  (
-    select text_value from metadatavalue where item_id=i.item_id and metadata_field_id =
-      (select metadata_field_id from metadatafieldregistry where element='title' and qualifier is null)
-  ) title
+
+  array_to_string(array(
+    select text_value from metadatavalue where resource_type_id=$TYPE_ITEM and resource_id=i.item_id and metadata_field_id=
+      (select metadata_field_id from metadatafieldregistry where element='title' and qualifier is null and metadata_schema_id=
+        (select metadata_schema_id from metadataschemaregistry where short_id='dc')
+      )
+  ), '||') titles
 
 from
   (
     select item_id from collection2item where collection_id in
-      (select resource_id from handle where resource_type_id=3 and handle='$collection_hdl')
+      (select resource_id from handle where resource_type_id=$TYPE_COLLECTION and handle='$collection_hdl')
   ) i
 order by 2
 "
@@ -44,7 +82,7 @@ with
   delimiter ','
   csv
     header
-    force quote handle,identifier,item_id,title
+    force quote handle,identifiers,item_id,titles
 "
 
 descr="Show the handle and identifier of every item in the specified collection.
@@ -53,9 +91,7 @@ original (Filemaker) database.
 "
 
 ##############################################################################
-psql_opts="-U $user -d $db -A -c \"$query\""
-cmd="psql $psql_opts"
-
+cmd="psql $psql_connect_opts -A -c \"$query\""
 cat <<-EOF
 
 	DESCRIPTION: $descr
